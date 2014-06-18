8b5d4dc2817d9430447601b852a9429d6d9b262f
Brendan McMahon <brendan.mcmahon@tigerpistol.com.au>
2014-06-18 10:48:59 +1000

    Added IResponseFormatter.AsError extension method

---
+++ Nancy.CustomErrors/ResponseFormatterExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Nancy.CustomErrors
+{
+    public static class ResponseFormatterExtensions
+    {
+        public static Response AsError(this IResponseFormatter formatter, string message,
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError)
+        {
+            var serializer = formatter.Serializers.FirstOrDefault(s => s.CanSerialize("application/json"));
+
+            return new ErrorResponse(new Error {Message = message}, serializer).WithStatusCode(statusCode);
+        }
+    }
+}
