da446fd73a1e70d14b00e01370031fa578b9bc61
Brendan McMahon <bernos@gmail.com>
2015-01-26 12:58:05 +1100

    set up build scripts

---
+++ build.ps1
@@ -0,0 +1,43 @@
+Param(
+    [Parameter(Position=1,Mandatory=0)]
+    [string[]]$task_list = @(),
+
+	[Parameter()]
+    [string]$BuildMetaData
+)
+
+$build_file = 'default.ps1'
+
+# Properties for the psake build script
+$properties = @{
+
+    # Build configuration to use
+    "configuration" = "Release";
+
+    # Version number to use if running the Publish build task.
+    # This will be read from the command line args
+    "version"       = $version;
+
+    # Path to the solution file
+    "solution"      = 'Nancy.CustomErrors.sln';
+
+    # Folder containing source code
+    "source_folder" = '';
+
+    # Folder to output deployable packages to. This folder should be ignored
+    # from any source control, as we dont commit build artifacts to source
+    # control
+    "deploy_folder" = 'deploy';
+
+	# Build number metadata that will be appended to semver numers
+	"build_meta" = $BuildMetaData;
+	
+    "projects" = @(
+        "Nancy.CustomErrors")
+
+}
+
+
+import-module .\packages\psake.4.4.1\tools\psake.psm1
+
+invoke-psake $build_file $task_list -Properties $properties
---
+++ default.ps1
@@ -0,0 +1,65 @@
+Properties {
+    $projects = $null
+    $configuration = "Release"
+    $source_folder = $null
+    $solution = $null
+	$build_meta = $null
+}
+
+Task Default -Depends Build
+
+
+Task Publish -Depends Package {
+    $version = getVersionBase
+    
+    foreach($project in $projects) {
+        Get-ChildItem | Where-Object -FilterScript {
+            ($_.Name.Contains("$project.$version")) -and !($_.Name.Contains(".symbols")) -and ($_.Extension -eq '.nupkg')    
+        } | ForEach-Object {
+            exec { nuget push $_.FullName }
+        }
+    }
+}
+
+Task Package -Depends Test {
+    foreach($project in $projects) {
+        Get-ChildItem -Path "$project\*.csproj" | ForEach-Object {            
+            exec { nuget pack -sym $_.FullName -Prop Configuration=$configuration }
+        }        
+    }
+}
+
+Task Test -Depends Build {
+    Get-ChildItem $source_folder -Recurse -Include *Tests.csproj | % {
+        Exec { & ".\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe" "$($_.DirectoryName)\bin\$configuration\$($_.BaseName).dll" /noshadow }
+    }
+}
+
+Task Build -Depends Clean,Set-Versions {
+    Exec { msbuild "$solution" /t:Build /p:Configuration=$configuration } 
+}
+
+Task Clean {
+    Exec { msbuild "$solution" /t:Clean /p:Configuration=$configuration } 
+}
+
+Task Set-Versions {
+    $version = getVersionBase
+
+	if ($build_meta) {
+        "##teamcity[buildNumber '$version+$build_meta']" | Write-Host
+    } else {
+		"##teamcity[buildNumber '$version']" | Write-Host
+	}
+
+    Get-ChildItem -Recurse -Force | Where-Object { $_.Name -eq "AssemblyInfo.cs" } | ForEach-Object {
+        (Get-Content $_.FullName) | ForEach-Object {
+            ($_ -replace 'AssemblyVersion\(.*\)', ('AssemblyVersion("' + $version + '")')) -replace 'AssemblyFileVersion\(.*\)', ('AssemblyFileVersion("' + $version + '")')
+        } | Set-Content $_.FullName -Encoding UTF8
+    }    
+}
+
+function getVersionBase {
+    $versionInfo = (Get-Content "version.json") -join "`n" | ConvertFrom-Json
+    "$($versionInfo.major).$($versionInfo.minor).$($versionInfo.patch)";    
+}
